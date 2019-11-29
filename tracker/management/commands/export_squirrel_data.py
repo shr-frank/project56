5c6710efb359e283c9c73ef20cd6900cea1e4746
Frank <hs3153@columbia.edu>
2019-11-29 03:36:04 +0000

    created export management command and tried export

---
+++ tracker/management/commands/export_squirrel_data.py
@@ -0,0 +1,22 @@
+import datetime
+from tracker.models import Squirrel
+from django.core.management.base import BaseCommand
+import csv
+
+class Command(BaseCommand):
+    
+    args = '[appname.ModelName]'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('file', type=str)
+
+    def handle(self, *args, **kwargs):
+        x=kwargs['file']
+        from django.apps import apps
+        model = apps.get_model('tracker', 'Squirrel')
+        field_names = [f.name for f in model._meta.fields]
+        with open(x, 'w') as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            writer.writerow(field_names)
+            for instance in model.objects.all():
+                writer.writerow([getattr(instance, f) for f in field_names])
