83ca4e4f5bf32cedfbcc6118162935824d5ae8a0
Youngkeun Yoon <yy3013@columbia.edu>
2019-11-30 00:39:41 +0000

    Updated models and views

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add, name='add'),
+    path('sightings/add/', views.add, name='add'),
     path('sightings/', views.sightings,  name='sightings.html'),
+    path('map/',views.map,name='map'),
     path('<str:unique_squirrel_id>/',views.update,name='update')
 ]
